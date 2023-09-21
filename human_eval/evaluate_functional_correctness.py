fbcc4caee70a07cef13f03867a055ce834d89d8e
Rick Lamers <ricklamers@gmail.com>
2023-09-21 21:38:16 +0200

    Fix entrypoint, uncomment exec.

--- human_eval/evaluate_functional_correctness.py
+++ human_eval/evaluate_functional_correctness.py
@@ -7,7 +7,7 @@ from human_eval.evaluation import evaluate_functional_correctness
 
 def entry_point(
     sample_file: str,
-    k: str = "1,10,100",
+    k: tuple = (1,10,100),
     n_workers: int = 4,
     timeout: float = 3.0,
     problem_file: str = HUMAN_EVAL,
@@ -16,7 +16,7 @@ def entry_point(
     Evaluates the functional correctness of generated samples, and writes
     results to f"{sample_file}_results.jsonl.gz"
     """
-    k = list(map(int, k.split(",")))
+    k = list(k)
     results = evaluate_functional_correctness(sample_file, k, n_workers, timeout, problem_file)
     print(results)
 
